a22d87901352056098fa39bda69a6b53ba6a0487
haireus <62015534+haireus@users.noreply.github.com>
2021-10-26 18:27:47 +0700

    Section 5: webpack and babel config

--- section-4/app.js
+++ section-4/app.js
@@ -3,5 +3,17 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate() {
+console.log("beforeCreate()", this.message);
+  },
+  created(){
+    console.log("create", this.message);
+  },
+  beforeMount() {
+console.log("mount", this.$el);
+  },
+  mounted(){
+    console.log("mounted", this.$el);
   }
 }).mount('#app')
